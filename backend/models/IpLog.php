d72a42bf1a5616d30ba4683338b5d3e530594ff3
yrwga <546167337@qq.com>
2017-12-22 16:52:23 +0800

    IP限制 用于后台登录密码输错暴力破解记录

---
+++ backend/models/IpLog.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "w_ip_log".
+ *
+ * @property integer $id
+ * @property string $ip_all
+ * @property string $ip_res
+ * @property string $dodate
+ * @property integer $errsum
+ * @property string $usercard
+ * @property string $userpwd
+ */
+class IpLog extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'w_ip_log';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['errsum'], 'integer'],
+            [['ip_all', 'ip_res', 'dodate', 'usercard', 'userpwd'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'ip_all' => 'ip段',
+            'ip_res' => '当前ip',
+            'dodate' => '操作日期',
+            'errsum' => '错误次数',
+            'usercard' => '登录账号',
+            'userpwd' => '登录密码',
+        ];
+    }
+}
