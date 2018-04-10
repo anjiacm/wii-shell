dcd493e7ccfa9c91212cf539e3a75b9dda20ccc7
wuguangan_mac <546167337@qq.com>
2018-04-10 09:39:10 +0800

    main-local不修改 新增copy

---
+++ api/components/Behavior.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/1/19
+ * Time: 17:35
+ */
+
+namespace api\components;
+
+use yii;
+use yii\base\ActionFilter;
+class ApiBehavior  extends ActionFilter
+{
+    public function beforeAction ($action)
+    {
+    }
+}
